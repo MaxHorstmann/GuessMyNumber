cfdc910e47a0926ddc3d52ef6a690bae53692538
Max Horstmann <max.horstmann@gmail.com>
2014-09-18 14:37:12 -0400

    count attempts

--- guess.js
+++ guess.js
@@ -2,10 +2,11 @@ var guessApp = angular.module('guessApp', []);
 
 guessApp.controller('guessController', ['$scope', function($scope) {
 	$scope.newGame = function() {
-		$scope.max =  100;
+		$scope.max =  50;
 	    $scope.number = Math.floor($scope.max * Math.random());
 	    $scope.guessMin = 0;
 	    $scope.guessMax = $scope.max;
+	    $scope.attempts = 0;
 	};
 	$scope.newGame();
 
@@ -16,6 +17,7 @@ guessApp.controller('guessController', ['$scope', function($scope) {
 	$scope.check = function(index) {
 		if (index < $scope.number) $scope.guessMin = index;
 		else $scope.guessMax = index;
+		$scope.attempts++;
 	};
 
 
